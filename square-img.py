9edea3db92db66bfb700df2f3907dac552fe3fe4
Ryan Williams <ryan.williams@mssm.edu>
2020-06-15 16:40:46 +0000

    better square-img helper

---
+++ square-img.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+
+def main():
+  parser = ArgumentParser()
+  parser.add_argument('input')
+  parser.add_argument('output', nargs='?')
+  parser.add_argument('-m', '--method', default='contain', choices=['contain','cover'])
+  args = parser.parse_args()
+
+  input = args.input
+  output = args.output or input
+  method = args.method
+
+  if method == 'contain':
+    distort = '%[fx:max(w,h)]x%[fx:max(w,h)]-%[fx:max((h-w)/2,0)]-%[fx:max((w-h)/2,0)]'
+  elif method == 'cover':
+    distort = '%[fx:min(w,h)]x%[fx:min(w,h)]+%[fx:max((w-h)/2,0)]+%[fx:max((h-w)/2,0)]'
+
+  cmd = [
+    'convert',
+    input,
+    '+set','date:create','+set','date:modify',
+    '-virtual-pixel','none',
+    '-set','option:distort:viewport',
+    distort,
+    '-filter','point',
+    '-distort','SRT','0',
+    '+repage',
+    output,
+  ]
+
+  from subprocess import check_call
+  print(cmd)
+  check_call(cmd)
+
+
+if __name__ == '__main__':
+  main()
