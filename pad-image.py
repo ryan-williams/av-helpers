b45ae87ac74e9d2f87715812d4cfeb1ba886505f
Ryan Williams <ryan@runsascoded.com>
2022-08-07 09:24:49 -0400

    separate geometry, extend in pad-image.py

--- pad-image.py
+++ pad-image.py
@@ -2,18 +2,29 @@
 
 import click
 from os import makedirs
-from os.path import dirname
+from os.path import dirname, exists
 from subprocess import check_call
 
 
 @click.command()
 @click.option('-b', '--background', default='black')
-@click.option('-g', '--geometry', '<w>x<h>')
+@click.option('-e', '--extent', '<w>x<h>', help="Overall image size (defaults to -g/--geometry value)")
+@click.option('-g', '--geometry', '<w>x<h>', required=True, help="Resized image size")
 @click.argument('inpath')
 @click.argument('outpath')
-def main(background, geometry, inpath, outpath):
-    makedirs(dirname(outpath), exist_ok=True)
-    check_call([ 'convert', inpath, '-resize', geometry, '-background', background, '-gravity', 'center', '-extent', geometry, outpath, ])
+def main(background, extent, geometry, inpath, outpath):
+    outdir = dirname(outpath)
+    if outdir and not exists(outdir):
+        makedirs(outdir, exist_ok=True)
+    extent = extent or geometry
+    check_call([
+        'convert', inpath,
+        '-resize', geometry,
+        '-background', background,
+        '-gravity', 'center',
+        '-extent', extent,
+        outpath,
+    ])
 
 
 if __name__ == '__main__':
