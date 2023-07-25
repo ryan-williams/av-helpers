bef2325b2c3ec598dd21eb311b6fa61c5a994759
Ryan Williams <ryan@runsascoded.com>
2023-07-25 09:13:25 -0400

    update square-img.py, add make_gif

--- square-img.py
+++ square-img.py
@@ -1,41 +1,38 @@
 #!/usr/bin/env python
+import shlex
 
-from argparse import ArgumentParser
+import click
+from click import Choice
+from subprocess import check_call
 
 
-def main():
-    parser = ArgumentParser()
-    parser.add_argument('input')
-    parser.add_argument('output', nargs='?')
-    parser.add_argument('-b', '--background', default='white')
-    parser.add_argument('-m', '--method', default='contain', choices=['contain', 'cover'])
-    args = parser.parse_args()
-
-    input = args.input
-    output = args.output or input
-    background = args.background
-    method = args.method
-
-    if method == 'contain':
-        distort = '%[fx:max(w,h)]x%[fx:max(w,h)]-%[fx:max((h-w)/2,0)]-%[fx:max((w-h)/2,0)]'
-    elif method == 'cover':
-        distort = '%[fx:min(w,h)]x%[fx:min(w,h)]+%[fx:max((w-h)/2,0)]+%[fx:max((h-w)/2,0)]'
+@click.command()
+@click.option('-b', '--background', default='edge')
+@click.option('-g', '--geometry')
+@click.option('-m', '--method', default='contain', type=Choice(['contain', 'cover']))
+@click.argument('in_path')
+@click.argument('out_path', required=False)
+def main(background, geometry, method, in_path, out_path):
+    if not geometry:
+        if method == 'contain':
+            geometry = '%[fx:max(w,h)]x%[fx:max(w,h)]-%[fx:max((h-w)/2,0)]-%[fx:max((w-h)/2,0)]'
+        elif method == 'cover':
+            geometry = '%[fx:min(w,h)]x%[fx:min(w,h)]+%[fx:max((w-h)/2,0)]+%[fx:max((h-w)/2,0)]'
 
     cmd = [
-      'convert',
-      input,
-      '+set', 'date:create', '+set', 'date:modify',
-      '-virtual-pixel', background,
-      '-set', 'option:distort:viewport',
-      distort,
-      '-filter', 'point',
-      '-distort', 'SRT', '0',
-      '+repage',
-      output,
+        'convert',
+        in_path,
+        '+set', 'date:create', '+set', 'date:modify',
+        '-virtual-pixel', background,
+        '-set', 'option:distort:viewport',
+        geometry,
+        '-filter', 'point',
+        '-distort', 'SRT', '0',
+        '+repage',
+        out_path,
     ]
 
-    from subprocess import check_call
-    print(cmd)
+    print(shlex.join(cmd))
     check_call(cmd)
 
 
