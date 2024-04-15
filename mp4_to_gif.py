0118904f6ce0d4ab6b94525f3023ab70623c53c4
Ryan Williams <ryan@runsascoded.com>
2024-04-15 11:45:20 -0400

    fmt mp4_to_gif.py

--- mp4_to_gif.py
+++ mp4_to_gif.py
@@ -5,15 +5,14 @@ from argparse import ArgumentParser
 from os.path import splitext
 from subprocess import check_call
 
-
 parser = ArgumentParser()
-parser.add_argument('-f','--fps',default=10)
-parser.add_argument('-s','--start')
-parser.add_argument('-t','--to')
-parser.add_argument('-w','--width',default=800)
-parser.add_argument('-y','--overwrite',action='store_true')
+parser.add_argument('-f', '--fps', default=10)
+parser.add_argument('-s', '--start')
+parser.add_argument('-t', '--to')
+parser.add_argument('-w', '--width', default=800)
+parser.add_argument('-y', '--overwrite', action='store_true')
 parser.add_argument('input')
-parser.add_argument('output',nargs='?')
+parser.add_argument('output', nargs='?')
 
 args = parser.parse_args()
 fps = args.fps
@@ -27,24 +26,29 @@ output = args.output
 
 cmd = [
     'ffmpeg',
-    '-i',input
+    '-i', input
 ]
 
-if overwrite: cmd += ['-y']
-if start: cmd += ['-ss',str(start)]
-if to: cmd += ['-to',str(to)]
+if overwrite:
+    cmd += ['-y']
+if start:
+    cmd += ['-ss', str(start)]
+if to:
+    cmd += ['-to', str(to)]
 
 cmd += [
-    '-vf',f'fps={fps},scale={width}:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse',
-    '-loop','0'
+    '-vf', f'fps={fps},scale={width}:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse',
+    '-loop', '0'
 ]
 
 if not output:
     name, ext = splitext(input)
     output = name
     output += f'_{fps}fps'
-    if start: output += f'_ss{start}'
-    if to: output += f'_to{to}'
+    if start:
+        output += f'_ss{start}'
+    if to:
+        output += f'_to{to}'
     output += '.gif'
 
 cmd += [output]
